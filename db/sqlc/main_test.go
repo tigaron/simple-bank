ab9f92f28a97bec3d5d9e566f0dda421893f00c7
tigaron <suputra@mail.com>
2023-02-05 11:47:52 +0000

    feat: implemented db transaction for transfer and entries

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -15,15 +15,18 @@ var (
 	dbDriver    = "postgres"
 	dbSource    = fmt.Sprintf("postgresql://%s:%s@%s:%s/simple_bank?sslmode=disable", os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_PORT"))
 	testQueries *Queries
+	testDB      *sql.DB
 )
 
 func TestMain(m *testing.M) {
-	conn, err := sql.Open(dbDriver, dbSource)
+	var err error
+
+	testDB, err = sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
 
-	testQueries = New(conn)
+	testQueries = New(testDB)
 
 	os.Exit(m.Run())
 }
